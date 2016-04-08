799e92a3ccd2ed6b82274272935cd93fd1759da2
Max Bye <maxbye@gmail.com>
2016-04-08 09:24:19 +0100

    working on angular kindness app

--- angular/app/app.component.js
+++ angular/app/app.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core', './hero.service', './heroes.component', './hero-detail.component', './dashboard.component', 'angular2/router'], function(exports_1, context_1) {
+System.register(['angular2/core'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,63 +10,47 @@ System.register(['angular2/core', './hero.service', './heroes.component', './her
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, hero_service_1, heroes_component_1, hero_detail_component_1, dashboard_component_1, router_1;
-    var AppComponent;
+    var core_1;
+    var Kindness, AppComponent, KINDNESSLIST;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
-            },
-            function (hero_service_1_1) {
-                hero_service_1 = hero_service_1_1;
-            },
-            function (heroes_component_1_1) {
-                heroes_component_1 = heroes_component_1_1;
-            },
-            function (hero_detail_component_1_1) {
-                hero_detail_component_1 = hero_detail_component_1_1;
-            },
-            function (dashboard_component_1_1) {
-                dashboard_component_1 = dashboard_component_1_1;
-            },
-            function (router_1_1) {
-                router_1 = router_1_1;
             }],
         execute: function() {
+            Kindness = (function () {
+                function Kindness() {
+                }
+                return Kindness;
+            }());
+            exports_1("Kindness", Kindness);
             AppComponent = (function () {
                 function AppComponent() {
-                    this.title = 'Tour of Heroes';
+                    this.kindnesses = KINDNESSLIST;
+                    this.kindness = {
+                        id: 1,
+                        deed: 'Test action'
+                    };
+                    this.title = 'Kindness App';
                 }
                 AppComponent = __decorate([
                     core_1.Component({
                         selector: 'my-app',
-                        template: "\n    <h1>{{title}}</h1>\n    <nav>\n  <a [routerLink]=\"['Heroes']\">Heroes</a>\n  <a [routerLink]=\"['Dashboard']\">Dashboard</a>\n    </nav>\n  <router-outlet></router-outlet> \n  ",
-                        directives: [router_1.ROUTER_DIRECTIVES],
-                        providers: [hero_service_1.HeroService, router_1.ROUTER_PROVIDERS]
-                    }),
-                    router_1.RouteConfig([
-                        {
-                            path: '/heroes',
-                            name: 'Heroes',
-                            component: heroes_component_1.HeroesComponent
-                        },
-                        {
-                            path: '/dashboard',
-                            name: 'Dashboard',
-                            component: dashboard_component_1.DashboardComponent,
-                            useAsDefault: true
-                        },
-                        {
-                            path: '/detail/:id',
-                            name: 'HeroDetail',
-                            component: hero_detail_component_1.HeroDetailComponent
-                        }
-                    ]), 
+                        template: "\n  <h1>{{title}}</h1>\n  <h2>{{kindness.deed}} details!</h2>\n  <div><label>id: </label>{{kindness.id}}</div>\n  <div>\n    <label>name: </label>\n    <input [(ngModel)]=\"kindness.deed\" placeholder=\"deed\">\n  </div>\n  \n  <h2>My Actions</h2>\n<ul class=\"heroes\">\n  <li>\n    <!-- each hero goes here -->\n    <li *ngFor=\"#kindness of kindnesses\">\n    <span class=\"badge\">{{kindness.id}}</span> {{kindness.deed}}\n  </li>\n</ul>\n\n  ",
+                        styles: ["\n  .selected {\n    background-color: #CFD8DC !important;\n    color: white;\n  }\n  .heroes {\n    margin: 0 0 2em 0;\n    list-style-type: none;\n    padding: 0;\n    width: 15em;\n  }\n  .heroes li {\n    cursor: pointer;\n    position: relative;\n    left: 0;\n    background-color: #EEE;\n    margin: .5em;\n    padding: .3em 0;\n    height: 1.6em;\n    border-radius: 4px;\n  }\n  .heroes li.selected:hover {\n    background-color: #BBD8DC !important;\n    color: white;\n  }\n  .heroes li:hover {\n    color: #607D8B;\n    background-color: #DDD;\n    left: .1em;\n  }\n  .heroes .text {\n    position: relative;\n    top: -3px;\n  }\n  .heroes .badge {\n    display: inline-block;\n    font-size: small;\n    color: white;\n    padding: 0.8em 0.7em 0 0.7em;\n    background-color: #607D8B;\n    line-height: 1em;\n    position: relative;\n    left: -1px;\n    top: -4px;\n    height: 1.8em;\n    margin-right: .8em;\n    border-radius: 4px 0 0 4px;\n  }\n"]
+                    }), 
                     __metadata('design:paramtypes', [])
                 ], AppComponent);
                 return AppComponent;
             }());
             exports_1("AppComponent", AppComponent);
+            KINDNESSLIST = [
+                { "id": 11, "deed": "Test Action 1" },
+                { "id": 12, "deed": "Test Action 2" },
+                { "id": 13, "deed": "Test Action 3" },
+                { "id": 14, "deed": "Test Action 4" },
+                { "id": 15, "deed": "Test Action 5" },
+            ];
         }
     }
 });
--- angular/app/app.component.ts
+++ angular/app/app.component.ts
@@ -1,43 +1,95 @@
-import { Component }       from 'angular2/core';
-import { HeroService }     from './hero.service';
-import { HeroesComponent } from './heroes.component';
-import { HeroDetailComponent } from './hero-detail.component';
-import { DashboardComponent } from './dashboard.component';
-import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from 'angular2/router';
+import {Component} from 'angular2/core';
+
+export class Kindness {
+  id: number;
+  deed: string;
+}
 
 @Component({
   selector: 'my-app',
-  template: `
-    <h1>{{title}}</h1>
-    <nav>
-  <a [routerLink]="['Heroes']">Heroes</a>
-  <a [routerLink]="['Dashboard']">Dashboard</a>
-    </nav>
-  <router-outlet></router-outlet> 
-  `,//
-  directives: [ROUTER_DIRECTIVES],
-  providers: [HeroService, ROUTER_PROVIDERS]
+  template:`
+  <h1>{{title}}</h1>
+  <h2>{{kindness.deed}} details!</h2>
+  <div><label>id: </label>{{kindness.id}}</div>
+  <div>
+    <label>name: </label>
+    <input [(ngModel)]="kindness.deed" placeholder="deed">
+  </div>
+  
+  <h2>My Actions</h2>
+<ul class="heroes">
+  <li>
+    <!-- each hero goes here -->
+    <li *ngFor="#kindness of kindnesses">
+    <span class="badge">{{kindness.id}}</span> {{kindness.deed}}
+  </li>
+</ul>
+
+  `,
+  styles:[`
+  .selected {
+    background-color: #CFD8DC !important;
+    color: white;
+  }
+  .heroes {
+    margin: 0 0 2em 0;
+    list-style-type: none;
+    padding: 0;
+    width: 15em;
+  }
+  .heroes li {
+    cursor: pointer;
+    position: relative;
+    left: 0;
+    background-color: #EEE;
+    margin: .5em;
+    padding: .3em 0;
+    height: 1.6em;
+    border-radius: 4px;
+  }
+  .heroes li.selected:hover {
+    background-color: #BBD8DC !important;
+    color: white;
+  }
+  .heroes li:hover {
+    color: #607D8B;
+    background-color: #DDD;
+    left: .1em;
+  }
+  .heroes .text {
+    position: relative;
+    top: -3px;
+  }
+  .heroes .badge {
+    display: inline-block;
+    font-size: small;
+    color: white;
+    padding: 0.8em 0.7em 0 0.7em;
+    background-color: #607D8B;
+    line-height: 1em;
+    position: relative;
+    left: -1px;
+    top: -4px;
+    height: 1.8em;
+    margin-right: .8em;
+    border-radius: 4px 0 0 4px;
+  }
+`]
 })
 
-@RouteConfig([
-  {
-    path: '/heroes',
-    name: 'Heroes',
-    component: HeroesComponent
-  },
-  {
-  path: '/dashboard',
-  name: 'Dashboard',
-  component: DashboardComponent,
-  useAsDefault: true
-},
-{
-  path: '/detail/:id',
-  name: 'HeroDetail',
-  component: HeroDetailComponent
-}
-])
-
 export class AppComponent {
-  title = 'Tour of Heroes';
+  public kindnesses = KINDNESSLIST;
+  kindness: Kindness = {
+    id: 1,
+    deed: 'Test action'
+  };
+  title = 'Kindness App';  
 }
+
+  var KINDNESSLIST: Kindness[] = [
+  { "id": 11, "deed": "Test Action 1" },
+  { "id": 12, "deed": "Test Action 2" },
+  { "id": 13, "deed": "Test Action 3" },
+  { "id": 14, "deed": "Test Action 4" },
+  { "id": 15, "deed": "Test Action 5" },
+];
